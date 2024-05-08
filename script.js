60f72bfbeaa36bdf360926f0909854d37b34d41b
ABHIRAJ9064 <159516528+ABHIRAJ9064@users.noreply.github.com>
2024-05-08 19:28:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+//your JS code here. If required.
+function displayHelloWorld() {
+  alert("Hello World!");
+}
+
+// Call the function to display the alert
+displayHelloWorld();
